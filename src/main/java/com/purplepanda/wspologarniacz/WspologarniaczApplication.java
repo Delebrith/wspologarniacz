ce6c8e761637d99ee0777817601a40f16da5bf90
Delebrith <paulina.szwed.1996@gmail.com>
2018-08-31 19:12:33 +0200

    Initial commit

---
+++ src/main/java/com/purplepanda/wspologarniacz/WspologarniaczApplication.java
@@ -0,0 +1,12 @@
+package com.purplepanda.wspologarniacz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WspologarniaczApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WspologarniaczApplication.class, args);
+	}
+}
---
+++ src/test/java/com/purplepanda/wspologarniacz/WspologarniaczApplicationTests.java
@@ -0,0 +1,16 @@
+package com.purplepanda.wspologarniacz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WspologarniaczApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
